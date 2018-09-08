1063d563e104c5c7a876aec9aa314a5e62cb11f0
Vincent Bitter <v.bitter@maxiblue.nl>
2018-09-08 20:43:41 +0200

    Add base methods to DependencyCollection

--- DependencySorter/DependencyCollection.cs
+++ DependencySorter/DependencyCollection.cs
@@ -1,6 +1,25 @@
-﻿namespace DependencySorter
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DependencySorter
 {
-    public class DependencyCollection
+    public class DependencyCollection<T> : IEnumerable<T>
     {
+        private readonly IDictionary<T, IList<T>> _items;
+
+        public DependencyCollection()
+        {
+            _items = new Dictionary<T, IList<T>>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.Keys.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
